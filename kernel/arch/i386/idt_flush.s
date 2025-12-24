85985f8eec1b7c78e5759508508e5b7949355195
Nadeesha Fernando <itsme@nadeeshafdo.dev>
2025-12-24 21:00:17 +0530

    chore: remove minimalos project files

--- kernel/arch/i386/idt_flush.s
+++
@@ -1,9 +0,0 @@
-.section .text
-.global idt_flush
-.type idt_flush, @function
-
-/* Load IDT */
-idt_flush:
-    mov 4(%esp), %eax    /* Get IDT pointer from stack */
-    lidt (%eax)          /* Load IDT */
-    ret
