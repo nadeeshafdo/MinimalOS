0bc8f5aa1070515ca08a80482cc1ff081cc14850
Nadeesha Fernando <itsme@nadeeshafdo.dev>
2025-12-28 12:36:19 +0530

    chore: remove MinimalOS project files (I think I have a better plan)

--- src/kernel/arch/x86_64/syscall.h
+++
@@ -1,9 +0,0 @@
-#ifndef SYSCALL_H
-#define SYSCALL_H
-
-#include "../../include/types.h"
-
-void syscall_init(void);
-void syscall_set_kernel_stack(uintptr stack_top);
-
-#endif // SYSCALL_H
--- src/kernel/lib/printk.h
+++
@@ -1,8 +0,0 @@
-#ifndef PRINTK_H
-#define PRINTK_H
-
-#include "../include/types.h"
-
-void printk(const char* fmt, ...);
-
-#endif // PRINTK_H
