576cc9502b59490db0d2de2e663aadadec4a871d
Nadeesha Fernando <itsme@nadeeshafdo.dev>
2026-02-17 09:09:29 +0530

    feat: add breakpoint handler to IDT

--- kernel/src/traps/idt.rs
+++ kernel/src/traps/idt.rs
@@ -1,7 +1,9 @@
 //! IDT initialization and management.
 
-use crate::arch::idt::Idt;
+use crate::arch::idt::{Idt, EntryOptions, GateType};
 use spin::Once;
+
+use super::handlers;
 
 /// Global IDT instance.
 ///
@@ -17,7 +19,28 @@ static IDT: Once<Idt> = Once::new();
 /// vectors (which will be added in subsequent achievements).
 pub fn init_idt() {
     // Create a new IDT with all entries marked as missing
-    let idt = Idt::new();
+    let mut idt = Idt::new();
+
+    // [020] Register breakpoint handler (INT 3)
+    let breakpoint_options = EntryOptions::new()
+        .set_present(true)
+        .set_gate_type(GateType::Interrupt);
+    
+    let handler_fn: extern "x86-interrupt" fn(x86_64::structures::idt::InterruptStackFrame) = handlers::breakpoint_handler;
+    let handler_addr = handler_fn as usize;
+    
+    // Get the current code segment selector from CS register
+    let cs: u16;
+    unsafe {
+        core::arch::asm!("mov {:x}, cs", out(reg) cs);
+    }
+    
+    idt.set_handler(
+        3, // Breakpoint vector
+        handler_addr,
+        cs, // Current kernel code segment
+        breakpoint_options,
+    );
 
     // Initialize the global IDT
     let idt_ref = IDT.call_once(|| idt);
