4c2ff78cc8bf9b3ce362009fe4fc5b1add4b6817
Nadeesha Fernando <itsme@nadeeshafdo.dev>
2026-02-17 09:09:38 +0530

    feat(traps): add function to trigger breakpoint exception

--- kernel/src/traps/mod.rs
+++ kernel/src/traps/mod.rs
@@ -1,5 +1,14 @@
 //! Interrupt and trap handling.
 
+mod handlers;
 mod idt;
 
 pub use idt::init_idt;
+
+/// Trigger a breakpoint exception for testing.
+#[inline]
+pub fn trigger_breakpoint() {
+    unsafe {
+        core::arch::asm!("int3");
+    }
+}
