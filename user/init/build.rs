2775f29299c0e7ddb43941b5f32ff0ad68dc0cc8
Nadeesha Fernando <itsme@nadeeshafdo.dev>
2026-02-18 10:27:19 +0530

    feat(user/init): add build.rs for flat binary linker script

---
+++ user/init/build.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let build_dir = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .expect("user/init must have parent")
+        .parent()
+        .expect("user must have parent")
+        .join("build");
+    println!("cargo:rustc-link-search=native={}", build_dir.display());
+    println!("cargo:rustc-link-arg=-Tlinker-user.ld");
+    println!("cargo:rerun-if-changed=../../build/linker-user.ld");
+}
